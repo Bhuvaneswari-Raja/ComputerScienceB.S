8bd15ba5ed482df2848e819b7d3555adf80a6409
Pooja Rajamaniakandan <rajamanikandanpooja@gmail.com>
2023-10-26 15:37:10 -0400

    Finished lab 7

--- Labs/lab07/favourite_nums.py
+++ Labs/lab07/favourite_nums.py
@@ -4,7 +4,8 @@ def print_favorite_numbers(who, favorites):
     :param favorites: the favorite numbers dictionary
     :return: None
     """
-    pass
+    print(favorites[who])
+    #pass
 
 
 def add_favorite_number(who, number, favorites):
@@ -15,18 +16,37 @@ def add_favorite_number(who, number, favorites):
     :param favorites: the favorites dictionary
     :return: None (dictionaries are mutable, so you don't need to return it to modify it)
     """
-    pass
+    if who not in favorites:
+        favorites[who] = list()
+        favorites[who].append(number)
+        print(number, "added to",who+"'s favourites")
+
+    elif who in favorites:        
+        if number in favorites[who]:
+            print(number, "was found in",who+"'s favourites")
+        else:
+            #favorites[who] = list()
+            favorites[who].append(number)
+            print(number, "added to",who+"'s favourites")
+        
+
+    #pass
 
 
 if __name__ == '__main__':
     favorites = {}
     in_string = input('What do you want to do (add favorite number), print favorite numbers for <person>, or quit? ')
+
     while in_string.strip().lower() != 'quit':
         if in_string.strip().lower().startswith('print favorite numbers for'):
             print_favorite_numbers(in_string.strip().split()[-1], favorites)
+
         if len(in_string.split()) == 2:
-            who, num = in_string.split()
+            who, num = in_string.split(" ")
+            #temp = in_string.split(" ")
+            #print(who)
             num = int(num)
             add_favorite_number(who, num, favorites)
 
+        #print(favorites)
         in_string = input('What do you want to do (add favorite number), or quit? ')
