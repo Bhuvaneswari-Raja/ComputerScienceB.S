2f25b3641bd174f55c8b3d4de2e2c3ca87f15b8d
Pooja Rajamaniakandan <rajamanikandanpooja@gmail.com>
2023-11-07 17:04:31 -0500

    edited binary.py in hw6

---
+++ Homework/hw06/binary.py
@@ -0,0 +1,15 @@
+"""
+File:    binary.py
+Author:  Pooja Rajamanikandan
+Date:    11/06/2023
+Section: 54
+E-mail:  le64534@umbc.edu
+Description: 
+"""
+
+def binary(n):
+    print(n)
+
+if __name__ == '__main__':
+   print()
+       
